95441cb6e8eb132c3d3d8f796ad18f2e85fd2fa6
Tom-Olav Bøyum <boyum90@gmail.com>
2019-01-10 22:24:02 +0100

    Changed colour of setup cells

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const CELL_COLOUR =  {
     living: 'black'
     , dead: "rgba(255, 255, 255, 0)"
     , visited: 'lightgreen'
-    , setup: '#ffffe5'
+    , setup: '#ccffcc'
 };
 
 const BACKGROUND_COLOUR = '#f2fcff';
