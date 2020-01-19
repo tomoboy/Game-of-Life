c6adb1dc50e6deb1e3445bd29f6a726b1dce3837
Tom-Olav Bøyum <boyum90@gmail.com>
2020-01-19 23:11:48 +0100

    playChords

--- src/board/drawFunctions.ts
+++ src/board/drawFunctions.ts
@@ -2,7 +2,7 @@ import { CELL_COLOUR } from '../colors';
 import { BoardState, ChangedState } from './types';
 import { RefObject } from 'react';
 import getNextGeneration from './gameLogic';
-import { playSoundNotes, playSoundChords } from "../sound";
+import { playSoundNotes, playSoundChords } from '../sound';
 
 export const getDrawFunctions = (
   canvasRef: RefObject<HTMLCanvasElement>,
@@ -11,7 +11,7 @@ export const getDrawFunctions = (
 ) => {
   const drawSquare = (x: number, y: number, alive: boolean) => {
     const canvas = canvasRef.current as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
     const actualSize = tileSize - 1;
     ctx.fillStyle = alive ? CELL_COLOUR.living : CELL_COLOUR.dead;
 
@@ -35,7 +35,7 @@ export const getDrawFunctions = (
   };
 
   const drawNextGeneration = (boardState: BoardState) => {
-    const coordinates: Array<[number, number]> = []
+    const coordinates: Array<[number, number]> = [];
     getNextGeneration(boardState).forEach(({ y, x, alive }) => {
       boardState[y][x] = alive;
       drawSquare(x, y, alive);
@@ -44,7 +44,7 @@ export const getDrawFunctions = (
       }
     });
     /* Can be replaced with playSoundNotes different effect*/
-    playSoundNotes(coordinates, boardState.length, boardState[0].length);
+    playSoundChords(coordinates, boardState.length, boardState[0].length);
   };
 
   return {
