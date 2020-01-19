6eb10dfd484a56037df2a445c9be1376e903992d
vegardkd <vegardkd@ifi.uio.no>
2020-01-19 17:14:45 +0100

    Add polyphonic options for sound

--- src/board/drawFunctions.ts
+++ src/board/drawFunctions.ts
@@ -2,7 +2,7 @@ import { CELL_COLOUR } from '../colors';
 import { BoardState, ChangedState } from './types';
 import { RefObject } from 'react';
 import getNextGeneration from './gameLogic';
-import { playSound } from '../sound';
+import { playSoundNotes, playSoundChords } from "../sound";
 
 export const getDrawFunctions = (
   canvasRef: RefObject<HTMLCanvasElement>,
@@ -11,7 +11,7 @@ export const getDrawFunctions = (
 ) => {
   const drawSquare = (x: number, y: number, alive: boolean) => {
     const canvas = canvasRef.current as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     const actualSize = tileSize - 1;
     ctx.fillStyle = alive ? CELL_COLOUR.living : CELL_COLOUR.dead;
 
@@ -19,7 +19,7 @@ export const getDrawFunctions = (
       ctx.clearRect(x * tileSize, y * tileSize, actualSize, actualSize);
     }
     ctx.fillRect(x * tileSize, y * tileSize, actualSize, actualSize);
-    if (alive && isSoundOn) playSound(x, y);
+    /*if (alive && isSoundOn) playSound(x, y);*/
   };
 
   const drawFullBoard = (boardState: BoardState) => {
@@ -35,10 +35,16 @@ export const getDrawFunctions = (
   };
 
   const drawNextGeneration = (boardState: BoardState) => {
+    const coordinates: Array<[number, number]> = []
     getNextGeneration(boardState).forEach(({ y, x, alive }) => {
       boardState[y][x] = alive;
       drawSquare(x, y, alive);
+      if (alive) {
+        coordinates.push([x, y])
+      };
     });
+    /* Can be replaced with playSoundNotes different effect*/
+    playSoundNotes(coordinates, boardState.length, boardState[0].length);
   };
 
   return {
--- src/sound.tsx
+++ src/sound.tsx
@@ -1,12 +1,65 @@
-import Tone from 'tone';
+import Tone from "tone";
 const synth = new Tone.Synth().toMaster();
+const poly = new Tone.PolySynth(6, Tone.Synth).toMaster();
 
 const NOTES = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'Ab'];
+const DIATONIC_NOTES = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
-const getNote = (y: number): string =>
-  `${NOTES[y % 11]}${Math.floor((y % 88) / 12)}`;
+const cmaj7 = ["C", "E", "G", "B"]
+const fmaj7 = ["F", "A", "C", "E"]
+const g7 = ["G", "B", "D", "F"]
+const dm7 = ["D", "F", "A", "C"]
+const em7 = ["E", "G", "B", "D"]
+const am7 = ["A", "C", "E", "G"]
+const bdim = ["B", "D", "F", "A"]
 
-export const playSound = (x: number, y: number) => {
-  const note = getNote(y);
-  synth.triggerAttackRelease(note, '16n');
+
+/* Used to count generations and chord index for playSoundChords*/
+var generations = 0
+var progression_index = 0
+
+
+const getNote = (y: number, rows: number, scale: Array<string>): string => {
+  /* scale defines which notes exist on the y axis*/
+  var octave = Math.floor((y % rows) / scale.length)
+  if (octave > 4) {octave = 4}
+  var note = `${scale[y % scale.length]}${octave}`;
+  return note
+
+}
+
+const playSoundNotes = (coordinates: Array<[number, number]>, rows: number, cols: number,) => {
+
+  var notes: any = coordinates.map((tuple) => {
+    return getNote(tuple[1], rows, DIATONIC_NOTES)
+  });
+
+  poly.triggerAttackRelease(notes.slice(0,5), '16n');
 };
+
+const playSoundChords = (coordinates: Array<[number, number]>, rows: number, cols: number,) => {
+  /* Example chord progression */
+  const progression = [am7, fmaj7, cmaj7, g7]
+
+  if ((generations % 8) === 0) {
+    if (progression_index === (progression.length - 1)) {
+      progression_index = 0
+    } else {
+      progression_index += 1
+    }
+  }
+  generations  += 1
+
+  var notes: any = coordinates.filter((tuple, index) => {
+    return ((index % 5) == 0)
+  }).map((tuple, index) => {
+    return getNote(tuple[1], rows, progression[progression_index])
+  });
+
+  poly.triggerAttackRelease(notes.slice(0,5), '16n');
+};
+
+export {
+  playSoundChords,
+  playSoundNotes
+}
